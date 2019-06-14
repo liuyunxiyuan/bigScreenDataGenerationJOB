a1aadb21e684b56e94aa2a09d2e69931303b4160
liuyunxiyuan <liuyun786536245@163.com>
2019-06-14 11:28:13 +0800

    create demo

---
+++ src/main/java/com/transun/App.java
@@ -0,0 +1,13 @@
+package com.transun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
